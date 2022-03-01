18cc4ed5d197b5c56c1ce0e2db843298122f07ee
Scaria <scaria1njoy99@gmail.com>
2022-03-01 21:52:01 +0530

    Row-working

--- src/Row.js
+++ src/Row.js
@@ -1,8 +1,10 @@
 import React, {useState, useEffect} from 'react';
-import axios from 'axios';
+import "./Row.css";
 import instance from './axios';
 
-function Row( {title, fetchUrl} ) {
+const base_url = "https://image.tmdb.org/t/p/original/"
+
+function Row( {title, fetchUrl, isLargeRow} ) {
   const [movies, setMovies] = useState([]);
 
   //runs everytime the row loads
@@ -11,19 +13,23 @@ function Row( {title, fetchUrl} ) {
     //By default, async calls cannot be called within useEffect, so a function is used
     async function fetchData(){
       const request = await instance.get(fetchUrl);
-      console.log(fetchUrl);
-      console.log(request);
+      setMovies(request.data.results)
       return request;
     }
     fetchData();
-  }, []);
-
+  }, [fetchUrl]);
 
   return (
-    <div>
+    <div className='row'>
         <h2>{title}</h2>
 
-        {/* container with posters */}
+        <div className='row__posters'>
+          {/* several row__posters */}
+
+          {movies.map(movie => (
+             <img key={movie.id} className={`row__poster ${isLargeRow && "row__posterLarge"}`} src={`${base_url}${isLargeRow? movie.poster_path : movie.backdrop_path}`} alt={movie.name} />
+          ))}
+        </div>
     </div>
   )
 }
