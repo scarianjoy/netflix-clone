2b4d26e41eab86c2cca7bb71349b40bdcfc176cd
Scaria <scaria1njoy99@gmail.com>
2022-03-02 00:16:36 +0530

    NavBar Completed

---
+++ src/Nav.js
@@ -0,0 +1,26 @@
+import React, { useState, useEffect } from 'react';
+import "./Nav.css";
+
+function Nav() { 
+    const [show, handleShow] = useState(false)
+
+  useEffect(() => {
+    window.addEventListener("scroll", () => {
+        if(window.scrollY> 100) {
+            handleShow(true);
+        } else handleShow(false);
+    });
+    return () => {
+        window.removeEventListener("Scroll")
+    };
+  }, [])
+
+  return (
+    <div className={`nav ${show && "nav__black"}`}>
+        <img className='nav__logo' src="https://upload.wikimedia.org/wikipedia/commons/0/08/Netflix_2015_logo.svg" alt="Netflix logo" />
+        <img className='nav__avatar' src="https://upload.wikimedia.org/wikipedia/commons/0/0b/Netflix-avatar.png?20201013161117" alt="Netflix logo" />
+    </div>
+  )
+}
+
+export default Nav
